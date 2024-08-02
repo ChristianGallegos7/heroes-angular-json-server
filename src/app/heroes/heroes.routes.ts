3397c7e7e4334b296c24e9e7b5d10902bbf3a6ad
Pxlarized <christian@moon.ec>
2024-08-01 23:55:28 -0500

    Servicio de Heroes ya realizado, y llamar el servicio desde ListPageComponent y mostrar los nombre de los heroes completado, falta diseño

--- src/app/heroes/heroes.routes.ts
+++ src/app/heroes/heroes.routes.ts
@@ -3,6 +3,7 @@ import { LayoutPageComponent } from "./layout-page/layout-page.component";
 import { NewPageComponent } from "./new-page/new-page.component";
 import { SearchPageComponent } from "./search-page/search-page.component";
 import { HeroePageComponent } from "./heroe-page/heroe-page.component";
+import { ListPageComponent } from "./list-page/list-page.component";
 
 //localhost:4200/heroes/
 export const heroesRoutes: Routes = [
@@ -13,7 +14,7 @@ export const heroesRoutes: Routes = [
             { path: 'new-hero', component: NewPageComponent },
             { path: 'search', component: SearchPageComponent },
             { path: 'edit/:id', component: NewPageComponent },
-            { path: 'list', component: NewPageComponent },
+            { path: 'list', component: ListPageComponent },
             { path: ':id', component: HeroePageComponent },
             { path: '**', redirectTo: 'list' }
         ]
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Heroe } from '../interfaces/heroe.interface';
+import { environments } from '../../../environments/environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
+  }
+}
